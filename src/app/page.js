427ed3601d60ced882dcec9a761a6b096ffa644f
Arvind <arvind@buddy4study.com>
2023-09-21 23:11:36 +0530

    add 3 new comp

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import {A} from './A';
+import {B} from './B'
+import {C} from './C'
 
 export default function App() {
   return (
-    <h1>This is export default component</h1>
+    <>
+    <A/>
+    <B/>
+    <C/>
+    </>
+    
   )
 }
